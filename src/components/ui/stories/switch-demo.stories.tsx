5a1c8cf00c7215fe55f2cfd41283e1e4d895fa75
jhean <brizadaojhean@gmail.com>
2023-11-25 19:34:47 -0300

    :memo: switch, swithWithform, stories

---
+++ src/components/ui/stories/switch-demo.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Label } from '../label'
+import { Switch } from '../switch'
+const meta = {
+  title: 'Shadcn/switch',
+  component: () => (
+    <div className="flex items-center space-x-2">
+      <Switch id="airplane-mode" />
+      <Label htmlFor="airplane-mode">Airplane Mode</Label>
+    </div>
+  ),
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<{}>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const SwitchDemo: Story = {
+  args: {},
+}
---
+++ src/components/ui/stories/switch-form-demo.stories.ts
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SwitchFormDemo } from '../demo/switch-form-demo'
+
+const meta = {
+  title: 'Shadcn/form',
+  component: SwitchFormDemo,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof SwitchFormDemo>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const SwitchForm: Story = {
+  args: {},
+}
