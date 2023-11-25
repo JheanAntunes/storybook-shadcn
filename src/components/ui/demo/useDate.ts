ce5c10661e6a9f48a7d5534e55eed3aed63e11c3
jhean <brizadaojhean@gmail.com>
2023-11-25 19:46:09 -0300

    feat: useDate e schemaForm reutilizaveis

---
+++ src/components/ui/demo/useDate.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function useDate() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+  return {
+    date,
+    setDate,
+  }
+}
+
+export default useDate
