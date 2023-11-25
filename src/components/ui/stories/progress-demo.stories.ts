67129b6e70a6be3f15e010bc3a25a6e0e7b36c1d
jhean <brizadaojhean@gmail.com>
2023-11-25 19:07:38 -0300

    :sparkles: :memo: progress, demo, stories

---
+++ src/components/ui/stories/progress-demo.stories.ts
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ProgressDemo } from '../demo/progress-demo'
+
+const meta = {
+  title: 'Shadcn/progress',
+  component: ProgressDemo,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof ProgressDemo>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Progress: Story = {
+  args: {},
+}
