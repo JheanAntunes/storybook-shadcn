2c0db24a708730a4199db7b26e7bb24b891e65b9
jhean <brizadaojhean@gmail.com>
2023-11-25 22:07:57 -0300

    feat: toast-demo

---
+++ src/components/ui/demo/toast-data-demo.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Button } from '../button'
+import { useToast } from '../use-toast'
+
+type ToastProps = {
+  title?: string
+  description?: string
+  variant?: 'destructive' | 'default'
+}
+export function ToastDemo({
+  description,
+  title,
+  variant = 'default',
+}: ToastProps) {
+  const { toast } = useToast()
+
+  return (
+    <Button
+      variant="outline"
+      onClick={() => {
+        toast({
+          title,
+          description,
+          variant,
+        })
+      }}
+    >
+      shadcn
+    </Button>
+  )
+}
