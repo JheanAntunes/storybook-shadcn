e3e44b5718e96efd064e366749761d4e07e16e8e
jhean <brizadaojhean@gmail.com>
2023-11-26 18:05:28 -0300

    docs: table demo

---
+++ src/components/ui/stories/table-demo.stories.ts
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { TableDemo } from '../demo/table-demo'
+
+const meta = {
+  title: 'Shadcn/table',
+  component: TableDemo,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof TableDemo>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+//render componente
+export const Table: Story = {
+  args: {},
+}
