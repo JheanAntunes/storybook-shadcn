15054672a8eb75f2756a54417ea2fd891e4dd3a2
jhean <brizadaojhean@gmail.com>
2023-12-01 18:14:44 -0300

    :sparkles: feat: props

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,54 +1,108 @@
-export function TypographyH1() {
+import { cn } from '../../../lib/utils'
+
+export function TypographyH1({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'h1'>) {
     return (
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Taxing Laughter: The Joke Tax Chronicles
+        <h1
+            className={cn(
+                'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+                className,
+            )}
+            {...props}
+        >
+            {children}
         </h1>
     )
 }
 
-export function TypographyH2() {
+export function TypographyH2({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'h2'>) {
     return (
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-            The People of the Kingdom
+        <h2
+            className={cn(
+                'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+                className,
+            )}
+            {...props}
+        >
+            {children}
         </h2>
     )
 }
 
-export function TypographyH3() {
+export function TypographyH3({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'h3'>) {
     return (
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-            The Joke Tax
+        <h3
+            className={cn(
+                'scroll-m-20 text-2xl font-semibold tracking-tight',
+                className,
+            )}
+            {...props}
+        >
+            {children}
         </h3>
     )
 }
 
-export function TypographyH4() {
+export function TypographyH4({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'h4'>) {
     return (
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-            People stopped telling jokes
+        <h4
+            className={cn(
+                'scroll-m-20 text-xl font-semibold tracking-tight',
+                className,
+            )}
+            {...props}
+        >
+            {children}
         </h4>
     )
 }
 
-export function TypographyP() {
+export function TypographyP({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'p'>) {
     return (
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-            The king, seeing how much happier his subjects were, realized the
-            error of his ways and repealed the joke tax.
+        <p
+            className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+            {...props}
+        >
+            {children}
         </p>
     )
 }
 
-export function TypographyBlockquote() {
+export function TypographyBlockquote({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'blockquote'>) {
     return (
-        <blockquote className="mt-6 border-l-2 pl-6 italic">
-            &quot;After all,&quot; he said, &quot;everyone enjoys a good joke,
-            so it&quot;s only fair that they should pay for the privilege.&quot;
+        <blockquote
+            className={cn('mt-6 border-l-2 pl-6 italic', className)}
+            {...props}
+        >
+            {children}
         </blockquote>
     )
 }
 
-export function TypographyTable() {
+export function TypographyTable(props: React.ComponentProps<'table'>) {
     return (
         <div className="my-6 w-full overflow-y-auto">
             <table className="w-full">
@@ -103,43 +157,77 @@ export function TypographyList() {
     )
 }
 
-export function TypographyInlineCode() {
+export function TypographyInlineCode({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'code'>) {
     return (
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-            @radix-ui/react-alert-dialog
+        <code
+            className={cn(
+                'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
+                className,
+            )}
+            {...props}
+        >
+            {children}
         </code>
     )
 }
 
-export function TypographyLead() {
+export function TypographyLead({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'p'>) {
     return (
-        <p className="text-xl text-muted-foreground">
-            A modal dialog that interrupts the user with important content and
-            expects a response.
+        <p
+            className={cn('text-xl text-muted-foreground', className)}
+            {...props}
+        >
+            {children}
         </p>
     )
 }
 
-export function TypographyLarge() {
+export function TypographyLarge({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'div'>) {
     return (
-        <div className="text-lg font-semibold">
-            Are you sure absolutely sure?
+        <div className={cn('text-lg font-semibold', className)} {...props}>
+            {children}
         </div>
     )
 }
 
-export function TypographySmall() {
+export function TypographySmall({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'small'>) {
     return (
-        <small className="text-sm font-medium leading-none">
-            Email address
+        <small
+            className={cn('text-sm font-medium leading-none', className)}
+            {...props}
+        >
+            {children}
         </small>
     )
 }
 
-export function TypographyMuted() {
+export function TypographyMuted({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<'p'>) {
     return (
-        <p className="text-sm text-muted-foreground">
-            Enter your email address.
+        <p
+            className={cn('text-sm text-muted-foreground', className)}
+            {...props}
+        >
+            {children}
         </p>
     )
 }
