b56bfbd053a0f60e36fbc163f418245b86d548ec
jhean <brizadaojhean@gmail.com>
2023-12-02 23:43:04 -0300

    :sparkles: feat: card create-account

---
+++ src/components/example/cards/components/create-account.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { Github, Goal } from 'lucide-react'
+import { Button } from '../../../ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '../../../ui/card'
+import { Input } from '../../../ui/input'
+import { Label } from '../../../ui/label'
+
+export function DemoCreateAccount() {
+    return (
+        <Card>
+            <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl">Create an account</CardTitle>
+                <CardDescription>
+                    Enter your email below to create your account
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+                <div className="grid grid-cols-2 gap-6">
+                    <Button variant="outline">
+                        <Github className="mr-2 h-4 w-4" />
+                        Github
+                    </Button>
+                    <Button variant="outline">
+                        <Goal className="mr-2 h-4 w-4" />
+                        Google
+                    </Button>
+                </div>
+                <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                        <span className="w-full border-t" />
+                    </div>
+                    <div className="relative flex justify-center text-xs uppercase">
+                        <span className="bg-background px-2 text-muted-foreground">
+                            Or continue with
+                        </span>
+                    </div>
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                    />
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" type="password" />
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Button className="w-full">Create account</Button>
+            </CardFooter>
+        </Card>
+    )
+}
