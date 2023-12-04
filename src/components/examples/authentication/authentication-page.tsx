6e10ada383a1c84e291a67b90feb5cc6383efbe6
jhean <brizadaojhean@gmail.com>
2023-12-04 20:00:06 -0300

    :recycle: refactor example authentication href link renamed to #

--- src/components/examples/authentication/authentication-page.tsx
+++ src/components/examples/authentication/authentication-page.tsx
@@ -27,7 +27,7 @@ export default function AuthenticationPage() {
             </div>
             <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
                 <Link
-                    href="/examples/authentication"
+                    href="#"
                     className={cn(
                         buttonVariants({ variant: 'ghost' }),
                         'absolute right-4 top-4 md:right-8 md:top-8',
@@ -77,14 +77,14 @@ export default function AuthenticationPage() {
                         <p className="px-8 text-center text-sm text-muted-foreground">
                             By clicking continue, you agree to our{' '}
                             <Link
-                                href="/terms"
+                                href="#"
                                 className="underline underline-offset-4 hover:text-primary"
                             >
                                 Terms of Service
                             </Link>{' '}
                             and{' '}
                             <Link
-                                href="/privacy"
+                                href="#"
                                 className="underline underline-offset-4 hover:text-primary"
                             >
                                 Privacy Policy
