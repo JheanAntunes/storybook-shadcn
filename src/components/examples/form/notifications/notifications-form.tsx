9d1eab9b2f5f61d59ef2f73fcd44c92d715abe07
jhean <brizadaojhean@gmail.com>
2023-12-04 21:47:52 -0300

    :lipstick: feat: form max-w-3xl

--- src/components/examples/form/notifications/notifications-form.tsx
+++ src/components/examples/form/notifications/notifications-form.tsx
@@ -62,7 +62,10 @@ export function NotificationsForm() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8 max-w-3xl"
+            >
                 <FormField
                     control={form.control}
                     name="type"
